bf9f8f23bddc9e2120644808da28402150cb95e3
penrods <github@gpll.org>
2017-12-10 05:38:26 -0600

    Update to Apache 2.0 license, etc
    Minor updates to the pairing skill, new README.md

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# Pairing Skill
+## Pairing
+Connect your device to the Mycroft server
 
-## Usage:
-* `pair my device`
-* `register my device`
-* `pair my unit`
+## Description 
+The default backend to provide services for Mycroft users is
+[Home](https://home.mycroft.ai/).  Pairing a device with Home provides access
+to privacy-protecting Speech to Text, Wolfram Alpha and other such services,
+as well as easy configuration for all your Mycroft devices.
+
+## Examples 
+* "Pair my device" (happens automatically on first run if not paired already)
+
+## Credits 
+Mycroft AI
--- __init__.py
+++ __init__.py
@@ -1,20 +1,17 @@
-# Copyright 2016 Mycroft AI, Inc.
+# Copyright 2017 Mycroft AI Inc.
 #
-# This file is part of Mycroft Core.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-
 import time
 from threading import Timer
 from uuid import uuid4
@@ -34,7 +31,7 @@ class PairingSkill(MycroftSkill):
         self.last_request = None
         self.state = str(uuid4())
         self.delay = 10
-        self.expiration = 72000 # 20 hours
+        self.expiration = 72000  # 20 hours
         self.activator = None
         self.repeater = None
 
@@ -116,14 +113,11 @@ class PairingSkill(MycroftSkill):
         self.log.info("Pairing code: " + code)
         data = {"code": '. '.join(map(self.nato_dict.get, code))}
         self.speak_dialog("pairing.code", data)
-        
+
         # repeat instructions/code every 60 seconds (start to start)
         self.repeater = Timer(60, self.speak_code)
         self.repeater.daemon = True
         self.repeater.start()
-
-    def stop(self):
-        pass
 
     def shutdown(self):
         super(PairingSkill, self).shutdown()
